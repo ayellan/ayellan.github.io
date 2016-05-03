cbf0d101cf04b558ccfaadf9ae817719e5529e9f
ayellan <AllanTina@ALLANs-MacBook-Pro.local>
2016-05-02 23:55:00 -0700

    Final Project Rough Draft

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//	click on section 2 button
+	//	toggle #section2content to slide in and out
+
+	//	fade out background image
+
+//	click on section 3 button
+	//	toggle #section2content to slide in and out
+
+	//	fade out background image
+
+//	click on section 4 button
+	//	toggle #section2content to slide in and out
+
+	//	fade out background image
+
+//	click on #emailbutton
+
+	//	opens up email form
