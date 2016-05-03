e8e0ba1929d9e0670ec7dbd38dd08aed289fc403
ayellan <AllanTina@ALLANs-MacBook-Pro.local>
2016-05-03 15:04:30 -0700

    Final Project - Edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+
+//	Click on About
+	//	Skip to section 2
+
+//	Click on Work
+	//	Skip to section 3
+
+//	Click on Arts
+	//	Skip to section 4
+
+//	Click on Contact
+	//	Skip to Footer
+
+
+
 //	click on section 2 button
 	//	toggle #section2content to slide in and out
 
@@ -16,3 +31,8 @@
 //	click on #emailbutton
 
 	//	opens up email form
+
+
+//	Click on Home
+
+	// Skip to Section 1
